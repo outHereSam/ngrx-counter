9de1b52dbac59c3eaacccf33cbd9171351428bc6
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-20 15:20:38 +0000

    Prevents counter from going to the negatives

--- src/app/reducers/counter.reducer.ts
+++ src/app/reducers/counter.reducer.ts
@@ -6,6 +6,6 @@ export const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1),
+  on(decrement, (state) => (state > 0 ? state - 1 : state)),
   on(reset, (state) => 0)
 );
